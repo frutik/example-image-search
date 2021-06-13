b1474552192ce9d18d240489812091f559f0fda7
Andrew Kornilov <frutik@gmail.com>
2021-06-13 13:47:21 +0200

    Dockerize app

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
 FROM python:3.9
 
+WORKDIR /opt/image-search
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY main.py ./
+
 USER www-data:www-data
+
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,7 @@ app = FastAPI()
 @app.get("/status")
 def status():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
