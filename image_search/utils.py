82a7a20b0f1fefbc82babb17bff22519308e3c8b
Andrew Kornilov <frutik@gmail.com>
2021-06-13 19:01:41 +0200

    Added possibility to reload required files

--- image_search/utils.py
+++ image_search/utils.py
@@ -8,8 +8,8 @@ ANNOY_INDEX = 'test.ann'
 DOCUMENTS_INDEX = 'id2doc.csv'
 
 
-def ensure_required(credentials, region, bucket):
+def ensure_required(credentials, region, bucket, force=False):
     preload_files = (ANNOY_INDEX, DOCUMENTS_INDEX)
     for f in preload_files:
-        if not os.path.isfile(TMP_PATH + f):
+        if not os.path.isfile(TMP_PATH + f) or force:
             download_from_s3(credentials, region, bucket, TMP_PATH + f, 'annoy/' + f)
