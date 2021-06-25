4c16651939f955f76c3a411ed794b650531ec018
Andrew Kornilov <frutik@MacBook-Pro-Andrew.local>
2021-06-25 10:46:17 +0200

    wip

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+from annoy import AnnoyIndex
 from invoke import task
 from img2vec_pytorch import Img2Vec
 from tqdm.auto import tqdm
@@ -39,19 +40,18 @@ def build_vector(remote_file):
         pass
 
 @task
-def build_vectors(c, docs=False, bytecode=False, extra=''):
-    vectors_filename = '/mnt/data/vectors.ndjson'
-    if os.path.isfile(vectors_filename):
-        raise RuntimeError('Vectors file already exists. Please remove it first.')
-    raw = 'images2categories-train.json'
-    download_from_s3(
-        (aws_access_key_id, aws_secret_access_key),
-        aws_region,
-        aws_s3_bucket,
-        TMP_PATH + raw,
-        'annoy/' + raw)
-    raw_data = open(TMP_PATH + raw, 'r').readlines()
-    vectors_file = open(vectors_filename, 'a')
+def build_vectors(c, file, help={'file': "File to process. Full path."}):
+    DIR = os.path.dirname(file) + '/'
+    FILENAME = os.path.basename(file)
+    vectors_filename = DIR + FILENAME + '.vec'
+    # download_from_s3(
+    #     (aws_access_key_id, aws_secret_access_key),
+    #     aws_region,
+    #     aws_s3_bucket,
+    #     TMP_PATH + raw,
+    #     'annoy/' + raw)
+    raw_data = open(DIR + FILENAME, 'r').readlines()
+    vectors_file = open(vectors_filename, 'w')
     for line in tqdm(raw_data, total=len(raw_data)):
         data = json.loads(line)
         if 'vector' not in data:
@@ -61,3 +61,26 @@ def build_vectors(c, docs=False, bytecode=False, extra=''):
         vectors_file.write(f'{json.dumps(data)}\n')
         vectors_file.flush()
     vectors_file.close()
+
+
+def get_idx(path):
+    data = open(path, 'r')
+    hash = {}
+    for f in tqdm(data):
+        vvv = f.split(' | ')
+        hash[int(vvv[0])] = vvv[1]
+    return hash
+
+
+@task
+def build_evaluation(c, evaluation_file, index_file, source_file, help={'file': "File to process. Full path."}):
+    annoy = AnnoyIndex(512, 'angular')
+    annoy.load(index_file)
+    sources = get_idx(source_file)
+    evaluation_data = open(evaluation_file, 'r').readlines()
+    for line in tqdm(raw_data, total=len(evaluation_data)):
+        data = json.loads(line)
+        if 'vector' not in data:
+            similar = annoy.get_nns_by_vector(data['vector'], 5)
+            for s in similar:
+                print(sources[s])
